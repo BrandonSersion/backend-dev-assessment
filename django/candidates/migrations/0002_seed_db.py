0f0f8ddb72ae60e1264fecb7e8922897814c368f
BrandonSersion <SersionSoftware@protonmail.com>
2018-07-04 14:09:27 -0400

    First commit. Remove Rails files. Fix migration DateTime (Python ints can't start with 0).

--- django/candidates/migrations/0002_seed_db.py
+++ django/candidates/migrations/0002_seed_db.py
@@ -13,7 +13,7 @@ def seed_db(apps, schema_editor):
         name='Alan Cruz',
         years_exp=10,
         status='pending',
-        date_applied=datetime(2018, 06, 05, 11, 55, 42),
+        date_applied=datetime(2018, 6, 5, 11, 55, 42),
         reviewed=False,
         description='An accomplished legal professional with a myriad of experience and a stellar reputation for handling all facets of litigation and trial practice.  An analytical and strategic thinker with a firm understanding of the drivers and interests impacting litigation, the legal ramifications of the workplace disciplinary process and the overall dynamics of attorney-client relations. Licensed to practice in Pennsylvania, New Jersey and the District of Columbia.'
     )
@@ -22,7 +22,7 @@ def seed_db(apps, schema_editor):
         name='Brian Patel',
         years_exp=13,
         status='accepted',
-        date_applied=datetime(2018, 06, 02, 9, 55, 42),
+        date_applied=datetime(2018, 6, 2, 9, 55, 42),
         reviewed=True,
         description='I am a seasoned litigator in the South Florida area with 10 years worth of experience in construction, maritime and insurance defense. My current practice involves real estate (litigation and transactional) and family law as well. In addition to extensive in-court experience, I offer excellent legal research and writing skills and a dedicated focus on the timely delivery of high quality product.'
     )
@@ -31,7 +31,7 @@ def seed_db(apps, schema_editor):
         name='Sally Collins',
         years_exp=2,
         status='rejected',
-        date_applied=datetime(2018, 06, 05, 11, 55, 42),
+        date_applied=datetime(2018, 6, 5, 11, 55, 42),
         reviewed=True,
         description='Experience in business immigration with an emphasis on the preparation, review and analysis of immigrant (PERM, I-140 immigrant petitions and AOS applications) and nonimmigrant (L-1A, L-1B, H-1B, O-1) visa petitions and applications. Work directly with large multinational clients in a broad spectrum of global industries. Advise on mergers, acquisitions and corporate reorganizations issues to ensure compliance with immigration regulations. In charge of responding to NOIR, RFE and audits.'
 
@@ -41,7 +41,7 @@ def seed_db(apps, schema_editor):
         name='Wendy Santiago',
         years_exp=15,
         status='pending',
-        date_applied=datetime(2018, 05, 05, 11, 55, 42),
+        date_applied=datetime(2018, 5, 5, 11, 55, 42),
         reviewed= False,
         description=''
     )
@@ -50,7 +50,7 @@ def seed_db(apps, schema_editor):
         name='Adrienne Hopkins',
         years_exp=1,
         status='pending',
-        date_applied=datetime(2018, 04, 30, 21, 55, 42),
+        date_applied=datetime(2018, 4, 30, 21, 55, 42),
         reviewed=False,
         description='Business litigation and employment litigation attorney; former law review editor; worked at top firms in Dallas, Texas for seven years before forming a private practice.'
     )
@@ -59,7 +59,7 @@ def seed_db(apps, schema_editor):
         name='Kari Morgan',
         years_exp=1,
         status='pending',
-        date_applied=datetime(2018, 04, 30, 21, 55, 42),
+        date_applied=datetime(2018, 4, 30, 21, 55, 42),
         reviewed=False,
         description='I am an attorney with a diverse background in multiple areas of practice. I have counseled banks, corporations, and individuals at both state and federal levels.'
     )
